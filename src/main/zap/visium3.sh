693aea27ef3a5076d6079c99e4dc9873c1209b76
firmansyah-github <firmansyah.profess@gmail.com>
2023-12-12 17:52:30 +0700

    test visium

---
+++ src/main/zap/visium3.sh
@@ -0,0 +1,10 @@
+# created by the factor : Dec 11, 2023, 6:10:51 PM  
+#!/bin/sh
+set -x
+
+#!/bin/bash
+
+curl -X GET https://farmdemo.visiumlabs.com/api/devices \
+-H "Content-Type: application/json" \
+-H "X-VisiumFarm-Api-Key: xmOo8Z5tp6.d9DQNq6nhbC9vxy2tDr32tfVBxUl6wKhpxSAiHtE" 
+
