a175f84d88940dee045a7afe9805455e914ec2db
firmansyah-github <firmansyah.profess@gmail.com>
2023-12-13 14:33:21 +0700

    test

--- src/main/zap/visium3.sh
+++ src/main/zap/visium3.sh
@@ -1,10 +1,12 @@
-# created by the factor : Dec 11, 2023, 6:10:51 PM  
-#!/bin/sh
-set -x
-
 #!/bin/bash
 
-curl -X GET https://farmdemo.visiumlabs.com/api/devices \
+# Make the GET request and capture the JSON response
+response=$(curl -s -X GET https://farmdemo.visiumlabs.com/api/devices?os=Android \
 -H "Content-Type: application/json" \
--H "X-VisiumFarm-Api-Key: xmOo8Z5tp6.d9DQNq6nhbC9vxy2tDr32tfVBxUl6wKhpxSAiHtE" 
+-H "X-VisiumFarm-Api-Key: xmOo8Z5tp6.d9DQNq6nhbC9vxy2tDr32tfVBxUl6wKhpxSAiHtE")
 
+# Extract the 'id' attributes from the JSON response and format output with double quotes
+ids=$(echo "$response" | jq -r '.[].id' | awk '{ printf("\"%d\", ", $1) }' | sed 's/, $//')
+
+# Output the extracted 'id' attributes in the required format
+echo "$ids"
