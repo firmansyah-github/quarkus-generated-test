e3d47ba2df68bc0b1158c530c8ec7eaf4424ceea
firmansyah-github <firmansyah.profess@gmail.com>
2023-12-13 14:39:54 +0700

    test

--- src/main/zap/visium.sh
+++ src/main/zap/visium.sh
@@ -21,6 +21,22 @@ else
 fi
 
 
+# step 2: get all devide id of Android
+# Make the GET request and capture the JSON response
+response=$(curl -s -X GET https://farmdemo.visiumlabs.com/api/devices?os=Android \
+-H "Content-Type: application/json" \
+-H "X-VisiumFarm-Api-Key: xmOo8Z5tp6.d9DQNq6nhbC9vxy2tDr32tfVBxUl6wKhpxSAiHtE")
+
+# Extract the 'id' attributes from the JSON response and format output with double quotes
+ids=$(echo "$response" | jq -r '.[].id' | awk '{ printf("\"%d\", ", $1) }' | sed 's/, $//')
+
+# Output the extracted 'id' attributes in the required format
+echo "$ids"
+
+export ids=$ids
+
+
+# step 3: upload APK file and get the id of APK file
 # Set the necessary variables
 API_KEY="xmOo8Z5tp6.d9DQNq6nhbC9vxy2tDr32tfVBxUl6wKhpxSAiHtE"
 FILE_PATH="/var/jenkins_home/workspace/visium/Simple_Notepad_2.0.1_Apkpure.apk"
@@ -32,9 +48,7 @@ response=$(curl -s -X POST "$API_URL" \
 -F "file=@$FILE_PATH")
 
 # Extract the 'id' attribute using jq
-#appId=$(echo "$response" | jq -r '.id')
-# Extract the 'appId' attribute using grep, cut, and tr
-#appId=$(echo "$response" | grep -o '"id":[0-9]*' | cut -d':' -f2 | tr -d '"')
+
 appId=$(echo "$response" | grep -o '"id" : [0-9]*' | cut -d':' -f2 | tr -d ' ')
 
 
@@ -43,6 +57,7 @@ echo "The extracted appId is: $appId"
 
 export appId=$appId
 
+# step 4: Install the APK in all Android devices
 curl -X POST https://farmdemo.visiumlabs.com/api/apk/install \
 -H "Content-Type: application/json" \
 -H "X-VisiumFarm-Api-Key: xmOo8Z5tp6.d9DQNq6nhbC9vxy2tDr32tfVBxUl6wKhpxSAiHtE" \
@@ -54,6 +69,6 @@ curl -X POST https://farmdemo.visiumlabs.com/api/apk/install \
     }
   ],
   "deviceList": [
-    "9321f298"
+    '"$ids"'
   ]
 }'
